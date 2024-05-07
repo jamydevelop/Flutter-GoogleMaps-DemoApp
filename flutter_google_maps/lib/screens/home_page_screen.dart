70ab6291f85a5a261b007e07f99dcbaf2d96bfa3
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-05-07 14:22:22 +0800

    Profile Screen Added

--- flutter_google_maps/lib/screens/home_page_screen.dart
+++ flutter_google_maps/lib/screens/home_page_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'drawer_item_screens/profile_screen.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -17,12 +19,53 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: Drawer(
         child: ListView(
-          children: const [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Header Title'),
+          children: [
+            // const DrawerHeader(
+            //   decoration: BoxDecoration(
+            //     color: Colors.blue,
+            //   ),
+            //   child: Text('Header Title'),
+            // ),
+            ListTile(
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context)  => const ProfileScreen()),
+                );
+                //Navigator.pop(context);
+                
+              },
+            ),
+            ListTile(
+              title: const Text('Support'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('History'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+            ),
+            Container(
+              alignment: Alignment.center,
+              color: Colors.blue,
+              width: 100.0,
+              height: 100.0,
+              child: const Text('Home Page'),
             )
           ],
         ),
